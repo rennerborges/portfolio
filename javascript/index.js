be3d2cb4121ed55f9334fe666172e9f4ae7ebd22
Renner Borges <renner>
2020-09-19 14:04:19 -0300

    Ajustando height de acordo com o viewport

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,16 @@
 typerWriter();
+adaptHeightViewport();
+
+
+function adaptHeightViewport(){
+    actionAdapt();
+    window.addEventListener('resize', () => {
+        actionAdapt();
+    });
+
+    function actionAdapt(){
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+}
